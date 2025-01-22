85fa5cb4c9c7f7f779ce8a48b4421aabf903b651
panchomorle <eljuampimorales@gmail.com>
2025-01-22 00:06:42 -0300

    Sync almost working
    we still need to handle the elimination of
    the guest profile once the user is linked

--- src/components/syncprofile-modal/syncprofile-modal-server.tsx
+++ src/components/syncprofile-modal/syncprofile-modal-server.tsx
@@ -1,22 +1,27 @@
 import { createClient } from "@/utils/supabase/server";
 import SyncProfileModalClient from "./syncprofile-modal-client";
-import { cookies } from 'next/headers';
+import { supabaseAdmin } from "@/utils/supabase/service-role";
 
 export default async function SyncProfileModalServer() {
   const supabase = await createClient();
 
   const { data: { user }, error } = await supabase.auth.getUser();
-
   if (error || !user) {
     console.error("Error obteniendo usuario o usuario inexistente:", error?.message);
     return null; // No muestra nada si no hay usuario
   }
 
-  const cookieStore = await cookies();
-  const memberCookie = cookieStore.get('member');
-  const member: Member = memberCookie ? JSON.parse(memberCookie.value) : null;
-
+  const { data: member, error: memberError } = await supabaseAdmin.from("members").select().eq("email", user.email!).single();
+  if (memberError) {
+    console.error("Error al obtener miembro:", memberError.message);
+    return null; // No muestra nada si hay error al obtener el miembro
+  }
   if(!member || member.auth_user_uuid === user.id) return null; // Si el usuario no tiene perfil miembro, no se muestra el modal
-
+  const {data: guest, error: guestError} = await supabase.from("guests").select().eq("auth_user_uuid", user.id).single();
+  if(guestError) {
+    console.error("Error al obtener invitado:", guestError.message);
+    return null; // No muestra nada si hay error al obtener el invitado
+  }
+  if(guest.club_id!==0) return null; // Si el invitado ya tiene club, no se muestra el modal
   return <SyncProfileModalClient member={member}/>;
 }
