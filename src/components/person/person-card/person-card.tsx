5dd3a58db48c877f6167503308a0a52a1b954a7e
panchomorle <eljuampimorales@gmail.com>
2025-02-13 15:48:01 -0300

    profile dynamic page

--- src/components/person/person-card/person-card.tsx
+++ src/components/person/person-card/person-card.tsx
@@ -1,9 +1,4 @@
-const AVATARS: { [key: number]: string } = {
-    1: "/images/pathfinder-avatar.jpg",
-    2: "/images/counselor-avatar.jpg",
-    3: "/images/staff-avatar.jpg",
-    0: "/images/guest-avatar.jpg"
-};
+import AVATARS from "@/constants/avatars";
 
 interface Props {
     person: Person;
